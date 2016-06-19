639eb2c3196ad5ec1b1df0e19cfd54ff0f276dbc
buddhax <peter.krogel@freenet.de>
2016-06-19 21:08:58 +0000

    Created by Coding Ground Client

---
+++ New Project-20160619/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Xyz.h"
+
+using namespace std;
+
+int main()
+{
+//   cout << "Hello World" << endl; 
+
+    Xyz myXyz;
+    
+    myXyz.show_x();
+   
+   return 0;
+}
+
